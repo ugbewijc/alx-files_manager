1bd8f24d9847db52e5e8f0cfbb8c134ac5b21cfb
frenol021 <frenol021@gmail.com>
2023-09-04 10:58:16 -0700

    creating a new user to DB

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import AppController from '../controllers/AppController';
+import UserController from '../controllers/UserContoller';
 
 const router = Router();
 
@@ -28,3 +29,4 @@ router.use((request, response, next) => {
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UserController.postNew);
