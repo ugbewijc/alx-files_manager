1da67869bc1b5993dbab5024444bdab9a744d0e2
frenol021 <frenol021@gmail.com>
2023-09-04 10:38:00 -0700

    first api

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,27 @@ import AppController from '../controllers/AppController';
 
 const router = Router();
 
+router.use((request, response, next) => {
+  const paths = ['/connect'];
+  if (!paths.includes(request.path)) {
+    next();
+  } else if (!request.headers.authorization) {
+    response.status(401).json({ error: 'Unauthorized' }).end();
+  } else {
+    next();
+  }
+});
+
+router.use((request, response, next) => {
+  const paths = ['/disconnect', '/users/me', '/files'];
+  if (!paths.includes(request.path)) {
+    next();
+  } else if (!request.headers['x-token']) {
+    response.status(401).json({ error: 'Unauthorized' }).end();
+  } else {
+    next();
+  }
+});
+
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
